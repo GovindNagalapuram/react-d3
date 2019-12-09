0133938c6bb38ca90f55d0c03db3b0aedb024ebd
Govind <govind@rollinglogs.com>
2019-12-09 19:44:08 +0530

    new files added

--- src/components/lineChart/index.js
+++ src/components/lineChart/index.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import Moment from 'react-moment';
 import LineChart from './lineChart';
 import ToolTip from './toolTip';
 import InfoBox from './infoBox';
@@ -10,13 +11,73 @@ const LineIndex = () => {
     const [ hoverLoc, setHoverLoc ] = useState(null);
     const [ activePoint, SetActivePoint ] = useState(null);
 
+    const[add, setAdd] = useState(true);
+    const[value, setValue] = useState(0);
+    const[increment, setIncrement] = useState(1);
+    const[end, setEnd] = useState(4)
+
+
+
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const calculation = () => {
+         if (add === true && value <= end) {
+            setValue(value => value + increment)
+            if (value === end) {
+                setAdd(false)
+            }
+         }
+         else {
+            setAdd(false)
+            setValue(value => value - increment)
+            if (value === 1) {
+                setAdd(true)
+            }
+        }
+      }
+
+    const getData = () => {
+        const url = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+  
+        fetch(url).then( r => r.json())
+          .then((bitcoinData) => {
+            const sortedData = [];
+            let count = 0;
+            for (let date in bitcoinData.bpi){
+              sortedData.push({
+                // d: Moment(date).format('MMM/DD'),
+                p: bitcoinData.bpi[date].toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }),
+                x: count, //previous days
+                y: bitcoinData.bpi[date] // numerical price
+              });
+              count++;
+            }
+            setData(sortedData)
+            setFetchingData(false)
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+
     return(
         <div className="index-container">
+            <div onClick={calculation}>{value}</div>
             <h1>Bitcoin price chart for 30 days</h1>
-            .row
-            <LineChart/>
-            <ToolTip/>
-            <InfoBox/>
+            { 
+                !fetchingData?
+                    <InfoBox data={data} />
+                : null 
+            }
+            <LineChart
+                
+            />
+            <ToolTip
+            
+            />
         </div>
     )
 }
--- src/components/lineChart/infoBox.js
+++ src/components/lineChart/infoBox.js
@@ -1,4 +1,4 @@
-// cindesk API = https://api.coindesk.com/v1/bpi/historical/close.json
+// coindesk API = https://api.coindesk.com/v1/bpi/historical/close.json
 
 // The API returns an object that looks like this:
 //   “bpi”: {
@@ -11,12 +11,50 @@
 //   }
 // }
 
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import '../../assets/lineChart/infoBox.css';
 
-const InfoBox = () => {
+const InfoBox = (props) => {
+
+    const[currentPrice, setCurrentPrice] = useState(null);
+    const[monthChangeD, setMonthChangeD] = useState(null);
+    const[monthChangeP, setMonthChangeP] = useState(null);
+    const[updatedAt, setUpdatedAt] = useState(null);
+
+    // console.log(data);
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = () => {
+        const {data} = props;
+        const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+  
+        fetch(url).then(r => r.json())
+          .then((bitcoinData) => {
+            const price = bitcoinData.bpi.USD.rate_float;
+            const change = price - data[0].y;
+            const changeP = (price - data[0].y) / data[0].y * 100;
+            
+            setCurrentPrice(bitcoinData.bpi.USD.rate_float)
+            setMonthChangeD(change.toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }))
+            setMonthChangeP(changeP.toFixed(2) + '%')
+            setUpdatedAt(bitcoinData.time.updated)
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+
     return (
-        <h1>this is info box container.</h1>
+        <div>
+            <h1>this is info box container.</h1>
+            {currentPrice}
+            {monthChangeD}
+            {monthChangeP}
+            {updatedAt}
+        </div>
+
     )
 }
 
